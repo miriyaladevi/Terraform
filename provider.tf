6e848be486559f021843e0ba8880f3d379728d20
EC2 Default User <ec2-user@ip-172-31-0-13.ec2.internal>
2023-01-30 05:51:53 +0000

    terraform

---
+++ provider.tf
@@ -0,0 +1,10 @@
+#update provider
+#AWS as our provider
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA3MXSWSOXO4FLLBHF"
+  secret_key = "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"
+}
+
+# Resource definition
