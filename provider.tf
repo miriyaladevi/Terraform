18622a56d51238ec21a9ab45af23ffce9a08a469
miriyaladevi <113349770+miriyaladevi@users.noreply.github.com>
2023-02-06 17:49:44 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,8 +3,8 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3MXSWSOXO4FLLBHF"
-  secret_key = "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"
+  access_key = "AKIAZ2NGWKDGOS753ZXN"
+  secret_key = "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"
 }
 
 # Resource definition
